12098ecdd8f6b49ef440c8f5eaa77478e3d66ecc
CrisUni <christhofer_gutierrez@outlook.com>
2016-07-09 10:32:11 -0600

    Creaciòn de repositorio

---
+++ Diccionario Beta/Diccionario Beta/Form1.Designer.vb
@@ -0,0 +1,53 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits DevComponents.DotNetBar.Metro.MetroForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MetroButton1 = New MetroFramework.Controls.MetroButton()
+        Me.SuspendLayout()
+        '
+        'MetroButton1
+        '
+        Me.MetroButton1.Location = New System.Drawing.Point(104, 49)
+        Me.MetroButton1.Name = "MetroButton1"
+        Me.MetroButton1.Size = New System.Drawing.Size(75, 23)
+        Me.MetroButton1.TabIndex = 0
+        Me.MetroButton1.Text = "MetroButton1"
+        Me.MetroButton1.UseSelectable = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(653, 436)
+        Me.Controls.Add(Me.MetroButton1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.Text = "MetroForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MetroButton1 As MetroFramework.Controls.MetroButton
+End Class
